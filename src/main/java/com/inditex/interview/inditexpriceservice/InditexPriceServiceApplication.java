77cc3370f35352184d07768ac0b6c4dc87f2e829
Kamil Makowski <kamil.makowski@yahoo.com>
2023-10-14 23:40:39 +0200

    initial commit

---
+++ src/main/java/com/inditex/interview/inditexpriceservice/InditexPriceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.interview.inditexpriceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexPriceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexPriceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/interview/inditexpriceservice/InditexPriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.interview.inditexpriceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexPriceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
